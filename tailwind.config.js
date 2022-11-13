abe72f067ea5a3debadb497d3bb2991ebcb85e01
mmorales <mt.morales@hotmail.fr>
2022-11-13 13:33:05 +0100

    configured tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#635fc7",
+      "hover-primary": "#a8a4ff",
+      secondary: "#f0effa",
+      "hover-secondary": "#d8d7f1",
+      danger: "#ea5555",
+      "hover-danger": "#ff9898",
+      white: "#fff",
+      black: "#000112",
+      body: "#828fa3",
+      shadow: "rgba(54, 78, 126, 0.1)",
+      light: "#f4f7fd",
+      dark: "#20212c",
+      "light-blue": "#e9effa",
+      placeholder: "rgba(0, 1, 18, 0.25)",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
