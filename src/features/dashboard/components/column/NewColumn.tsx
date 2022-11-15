f9415d7b369f55b0518517d21c24386cc0142e37
mmorales <mt.morales@hotmail.fr>
2022-11-15 14:22:21 +0100

    added gradient to new column component

--- src/features/dashboard/components/column/NewColumn.tsx
+++ src/features/dashboard/components/column/NewColumn.tsx
@@ -1,6 +1,6 @@
 export function NewColumn() {
   return (
-    <button className="mt-10 shrink-0 grow-0 basis-72 rounded-lg bg-light-blue text-2xl font-bold capitalize transition-colors hover:text-primary">
+    <button className="mt-10 shrink-0 grow-0 basis-72 rounded-lg bg-gradient-to-b from-light-blue text-2xl font-bold capitalize transition-colors hover:text-primary">
       new column
     </button>
   );
