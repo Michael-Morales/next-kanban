fb3a0847fed39c085ee7c20ea60fe126c7fa35d4
mmorales <mt.morales@hotmail.fr>
2022-11-23 15:32:10 +0100

    navigation: added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@features/ui";
+
+export default function PageNotFound() {
+  return (
+    <Layout>
+      <div className="flex h-[calc(100vh-60px)] flex-col items-center justify-center text-3xl font-bold uppercase tracking-wider md:h-[calc(100vh-68px)]">
+        board not found
+      </div>
+    </Layout>
+  );
+}
